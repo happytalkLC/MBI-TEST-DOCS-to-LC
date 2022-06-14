21f132d768da70ca76511412d8192fa7b63a132e
lc <lc@happytalk.io>
2022-06-14 16:22:27 +0900

    test

--- source/conf.py
+++ source/conf.py
@@ -31,9 +31,14 @@ release = 'latest'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'sphinxcontrib.mermaid'
+    'myst_parser'
 ]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
